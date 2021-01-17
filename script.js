764ca1e479dc0fba9356165aa589816a35db5fc3
Luan Santos <62774900+LuanSantos00@users.noreply.github.com>
2021-01-17 13:02:16 -0300

    Add files via upload
    My first application booting a camera with JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+function startVideoFromCamera(){
+
+    
+    navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
+
+        const videoElement = document.querySelector('#video');
+        videoElement.srcObject = stream;
+
+    }).catch(error =>{console.log(error)})
+
+
+}
+
+window.addEventListener("DOMContentLoaded", startVideoFromCamera);
